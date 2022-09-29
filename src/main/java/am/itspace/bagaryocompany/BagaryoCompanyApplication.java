8ef9c35de4cb2fdc97ca0daa8b59355c1cffe1a2
Paruyr <pash111190pash@mail.ru>
2022-09-29 15:58:46 +0400

    Initial commit

---
+++ src/main/java/am/itspace/bagaryocompany/BagaryoCompanyApplication.java
@@ -0,0 +1,13 @@
+package am.itspace.bagaryocompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BagaryoCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BagaryoCompanyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/am/itspace/bagaryocompany/BagaryoCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package am.itspace.bagaryocompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BagaryoCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
