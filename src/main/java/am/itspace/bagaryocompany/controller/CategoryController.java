a4e10c809068c21c6cb34e4a202dcbe7be7f58a0
Paruyr <pash111190pash@mail.ru>
2022-10-07 14:06:17 +0400

    category

--- src/main/java/am/itspace/bagaryocompany/controller/CategoryController.java
+++ src/main/java/am/itspace/bagaryocompany/controller/CategoryController.java
@@ -1,9 +1,22 @@
 package am.itspace.bagaryocompany.controller;
 
+import am.itspace.bagaryocompany.entity.Category;
+import am.itspace.bagaryocompany.repository.CategoryRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class CategoryController {
 
+    private CategoryRepository categoryRepository;
 
+    @GetMapping("/categories")
+    public String categoryPage(ModelMap modelMap) {
+        List<Category> categories = categoryRepository.findAll();
+        modelMap.addAttribute("categories", categories);
+        return "category";
+    }
 }
