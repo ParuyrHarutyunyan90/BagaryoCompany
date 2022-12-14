429b060542e0421a53723409c6f16d543ba46488
Armen Hambaryan <hambaryan.93@bk.ru>
2022-12-14 19:39:52 +0400

    init modular

--- bagario-company-rest/src/main/java/am/itspace/bagariocompanyrest/BagarioCompanyRestApplication.java
+++ bagario-company-rest/src/main/java/am/itspace/bagariocompanyrest/BagarioCompanyRestApplication.java
@@ -2,7 +2,13 @@ package am.itspace.bagariocompanyrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@ComponentScan({"am.itspace.bagariocompanycommon.*","am.itspace.bagariocompanyrest.*"})
+@EnableJpaRepositories(basePackages = {"am.itspace.bagariocompanycommon.repository"})
+@EntityScan({"am.itspace.bagariocompanycommon.entity"})
 @SpringBootApplication
 public class BagarioCompanyRestApplication {
 
--- bagario-company-web/src/main/java/am/itspace/bagariocompanyweb/BagarioCompanyWebApplication.java
+++ bagario-company-web/src/main/java/am/itspace/bagariocompanyweb/BagarioCompanyWebApplication.java
@@ -2,7 +2,13 @@ package am.itspace.bagariocompanyweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@ComponentScan({"am.itspace.bagariocompanycommon.*","am.itspace.bagariocompanyweb.*"})
+@EnableJpaRepositories(basePackages = {"am.itspace.bagariocompanycommon.repository"})
+@EntityScan({"am.itspace.bagariocompanycommon.entity"})
 @SpringBootApplication
 public class BagarioCompanyWebApplication {
 
